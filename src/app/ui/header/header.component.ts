49d5607ef4106dbd0094999d45de773be7a9a726
Klaus Ferreira <klauseverwalkingdev@yandex.com>
2023-10-09 20:52:47 -0300

    feature: create header component

---
+++ src/app/ui/header/header.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core'
+import {CommonModule} from '@angular/common'
+
+@Component({
+	selector: 'dcorg-header',
+	standalone: true,
+	imports: [CommonModule],
+	templateUrl: './header.component.html',
+	styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {}
