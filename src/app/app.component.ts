745e898a67567762b7b237d56fcf1e20864298fa
Klaus Ferreira <klauseverwalkingdev@yandex.com>
2023-10-22 21:18:46 -0300

    chore: update configurations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { CommonModule } from '@angular/common'
-import { Component } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
+import { ChangeDetectionStrategy, Component } from '@angular/core'
 
 import { GlobalConstants } from '@dcorg/core/constants/constants'
 
 @Component({
-	imports: [CommonModule, RouterOutlet],
+	changeDetection: ChangeDetectionStrategy.OnPush,
+	imports: [RouterOutlet],
 	selector: 'dcorg-app',
 	standalone: true,
 	styles: [],
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser'
-import { HttpClientModule } from '@angular/common/http'
-import { RouterModule } from '@angular/router'
+// eslint-disable-next-line import/named
+import { MarkdownModule } from 'ngx-markdown'
+import { provideAnimations } from '@angular/platform-browser/animations'
+import { provideHttpClient } from '@angular/common/http'
 import { type ApplicationConfig, importProvidersFrom } from '@angular/core'
+import {
+	PreloadAllModules,
+	provideRouter,
+	withComponentInputBinding,
+	withDebugTracing,
+	withPreloading
+} from '@angular/router'
 
 import { routes } from './app.routes'
 
 export const appConfig: ApplicationConfig = {
-	providers: [importProvidersFrom(BrowserModule, HttpClientModule, RouterModule.forRoot(routes))]
+	providers: [
+		provideRouter(
+			routes,
+			withComponentInputBinding(),
+			withPreloading(PreloadAllModules),
+			withDebugTracing()
+		),
+		importProvidersFrom(
+			BrowserModule,
+			MarkdownModule.forRoot()
+		),
+		provideAnimations(),
+		provideHttpClient()
+	]
 }
