9106a0caa6738dd87aaaed111338b877abf0c632
Klaus Ferreira <klauseverwalkingdev@yandex.com>
2023-10-30 00:04:27 -0300

    refactor: make article mapping more explicable

--- src/app/core/services/article.service.ts
+++ src/app/core/services/article.service.ts
@@ -15,18 +15,57 @@ export class ArticleService {
 	private urls = ['2021-07-07_e-nos-tornamos-devcaminhantes']
 
 	getArticles(): Observable<Article>[] {
-		let id = GlobalConstants.FIRST_ID
+		let id = GlobalConstants.FIRST_ARTICLE_ID
+		const articleContentFirstLineIndex = GlobalConstants.ARTICLE_CONTENT_FIRST_LINE_INDEX
+		const lessOrMoreOneWholeNumber = GlobalConstants.LESS_OR_MORE_ONE_WHOLE_NUMBER
 
 		return this.urls.map((url) => {
 			return this.httpClient.get(`assets/blog/articles/${url}.md`, { responseType: 'text' })
 				.pipe(map((string): Article => {
+					const articleLines = string.split('\n')
+					const titleLineStart = 'title: '
+					const dateLineStart = 'date: '
+					const authorLineStart = 'author: '
+
+					const titleLine = articleLines
+						.filter((line) => {
+							return line.includes(titleLineStart)
+						})
+						.join()
+					const dateLine = articleLines
+						.filter((line) => {
+							return line.includes(dateLineStart)
+						})
+						.join()
+					const authorLine = articleLines
+						.filter((line) => {
+							return line.includes(authorLineStart)
+						})
+						.join()
+					const contentLines = articleLines
+						.slice(articleContentFirstLineIndex, articleLines.length)
+						.join('\n')
+
+					const title = titleLine.substring(
+						titleLineStart.length + lessOrMoreOneWholeNumber,
+						titleLine.length - lessOrMoreOneWholeNumber
+					)
+					const date = dateLine.substring(
+						dateLineStart.length + lessOrMoreOneWholeNumber,
+						dateLine.length - lessOrMoreOneWholeNumber
+					)
+					const author = authorLine.substring(
+						authorLineStart.length + lessOrMoreOneWholeNumber,
+						authorLine.length - lessOrMoreOneWholeNumber
+					)
+
 					const parsedString = {
-						author: string.split('author: \'')[1].split('\'\n')[0],
-						content: string.split('author: ')[1].split(/-{3}\W+/u)[1],
-						date: string.split('date: \'')[1].split('\'\n')[0],
+						author,
+						content: contentLines,
+						date,
 						id,
 						slug: url,
-						title: string.split('title: \'')[1].split('\'\n')[0]
+						title
 					}
 
 					this.articleStateService.addArticle(parsedString)
