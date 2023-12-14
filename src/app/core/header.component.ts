8d3ea8e4bd5652e1e03531d2843fc04cd2bc981c
Klaus Ferreira <klauseverwalkingdev@yandex.com>
2023-12-14 16:12:21 -0300

    feature: create header

---
+++ src/app/core/header.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'dcorg-header',
+  standalone: true,
+  imports: [CommonModule],
+  template: `<p>header works!</p>`,
+  styles: ``,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class HeaderComponent {}
--- src/app/core/shell.component.ts
+++ src/app/core/shell.component.ts
@@ -1,12 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from "./header.component";
 
 @Component({
-  selector: 'dc-org-shell',
-  standalone: true,
-  imports: [CommonModule],
-  template: `<p>shell works!</p>`,
-  styles: ``,
-  changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'dcorg-shell',
+    standalone: true,
+    template: `<dcorg-header></dcorg-header>`,
+    styles: ``,
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    imports: [CommonModule, HeaderComponent]
 })
 export class ShellComponent {}
