83ff5fc2e233a48ea90d50ae7fd32739a6ffd8de
Klaus Ferreira <klauseverwalkingdev@yandex.com>
2023-12-14 22:11:49 -0300

    feature: create header primitive version

--- src/app/core/header.component.ts
+++ src/app/core/header.component.ts
@@ -5,7 +5,36 @@ import { CommonModule } from '@angular/common';
   selector: 'dcorg-header',
   standalone: true,
   imports: [CommonModule],
-  template: `<p>header works!</p>`,
+  template: `
+		<div class="header header-fixed u-unselectable header-animated header-dark bg-purple-dcorg">
+			<div class="header-brand">
+				<div class="nav-item no-hover">
+					<a>
+						<h6 class="title">DevCaminhante</h6>
+					</a>
+				</div>
+				<div class="nav-item nav-btn" id="header-btn">
+					<span></span>
+					<span></span>
+					<span></span>
+				</div>
+			</div>
+
+			<div class="header-nav" id="header-menu">
+				<div class="nav-left">
+					<div class="nav-item active">
+						<a href="#">Home</a>
+					</div>
+					<div class="nav-item">
+						<a href="#">Blog</a>
+					</div>
+					<div class="nav-item">
+						<a href="#">Sobre</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	`,
   styles: ``,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
