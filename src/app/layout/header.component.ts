a2bab7a5b4c471dd3065ce338f1d6b560f759d38
Klaus Ferreira <klauseverwalkingdev@yandex.com>
2023-12-28 10:46:15 -0300

    feature: add routerlinks to features

--- src/app/layout/header.component.ts
+++ src/app/layout/header.component.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {RouterLink, RouterLinkActive} from '@angular/router'
 
 @Component({
-  selector: 'dcorg-header',
-  standalone: true,
-  imports: [CommonModule],
-  template: `
+	selector: 'dcorg-header',
+	standalone: true,
+	imports: [CommonModule, RouterLink, RouterLinkActive],
+	template: `
 		<div class="header header-fixed u-unselectable header-animated header-dark bg-purple-dcorg">
 			<div class="header-brand">
 				<div class="nav-item no-hover">
@@ -23,19 +24,19 @@ import { CommonModule } from '@angular/common';
 			<div class="header-nav" id="header-menu">
 				<div class="nav-left">
 					<div class="nav-item active">
-						<a href="#">Home</a>
+						<a routerLink="/" routerLinkActive="active" ariaCurrentWhenActive="page">Home</a>
 					</div>
 					<div class="nav-item">
-						<a href="#">Blog</a>
+						<a routerLink="/blog" routerLinkActive="active" ariaCurrentWhenActive="page">Blog</a>
 					</div>
 					<div class="nav-item">
-						<a href="#">Sobre</a>
+						<a routerLink="/sobre" routerLinkActive="active" ariaCurrentWhenActive="page">Sobre</a>
 					</div>
 				</div>
 			</div>
 		</div>
 	`,
-  styles: ``,
-  changeDetection: ChangeDetectionStrategy.OnPush,
+	styles: ``,
+	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeaderComponent {}
