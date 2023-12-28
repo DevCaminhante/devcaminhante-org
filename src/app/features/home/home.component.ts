c51f001dc9402cbb5392838237af1b3d00c4fe2c
Klaus Ferreira <klauseverwalkingdev@yandex.com>
2023-12-28 08:27:44 -0300

    feature: create home feature

---
+++ src/app/features/home/home.component.ts
@@ -0,0 +1,20 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+	selector: 'dcorg-home',
+	standalone: true,
+	imports: [CommonModule],
+	template: `
+		<p>DevCaminhante &eacute; um projeto dedicado ao acolhimento tanto para 
+			quem est&aacute; pensando em entrar na &aacute;rea de programa&ccedil;&atilde;o 
+			como para quem est&aacute; come&ccedil;ando a aprender nesta &aacute;rea. 
+			Tamb&eacute;m recebe de bra&ccedil;os abertos os devs mais experientes 
+			que querem compartilhar conhecimento e tamb&eacute;m aprender, estimulando 
+			o crescimento m&uacute;tuo, sempre com a mentalidade que todos somos aprendizes. 
+			:)</p>
+	`,
+	styles: ``,
+	changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class HomeComponent {}
