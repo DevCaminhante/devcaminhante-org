5fc373bd7522647393df49771aea6aa036dca879
Klaus Ferreira <klauseverwalkingdev@yandex.com>
2023-12-28 10:44:06 -0300

    chore: update shell routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Route } from '@angular/router';
 
 import { ShellComponent } from './layout/shell.component'
+import { shellRoutes } from './layout/shell.routes'
 
 export const appRoutes: Route[] = [
 	{
 		path: '',
-		component: ShellComponent
+		component: ShellComponent,
+		children: shellRoutes,
 	}
 ];
---
+++ src/app/layout/shell.routes.ts
@@ -0,0 +1,22 @@
+import { Route } from '@angular/router';
+
+import { blogRoutes } from '../features/blog/blog.routes'
+import { HomeComponent } from '../features/home/home.component'
+
+export const shellRoutes: Route[] = [
+	{
+		path: '',
+		component: HomeComponent,
+	},
+	{
+		path: 'blog',
+		loadComponent: () => import('../features/blog/blog.component')
+			.then(m => m.BlogComponent),
+		children: blogRoutes,
+	},
+	{
+		path: 'sobre',
+		loadComponent: () => import('../features/sobre/sobre.component')
+			.then(m => m.SobreComponent),
+	}
+];
