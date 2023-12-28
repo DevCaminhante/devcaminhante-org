503d84ec7b7c79082b4d76173822771c2374cc76
Klaus Ferreira <klauseverwalkingdev@yandex.com>
2023-12-28 10:41:26 -0300

    feature: create blog routes

---
+++ src/app/features/blog/blog.routes.ts
@@ -0,0 +1,14 @@
+import { Route } from '@angular/router';
+
+export const blogRoutes: Route[] = [
+	{
+		path: '',
+		pathMatch: 'full',
+		redirectTo: 'articles',
+	},
+	{
+		path: 'articles',
+		loadComponent: () => import('./article-list/article-list.component')
+			.then(m => m.ArticleListComponent),
+	},
+];
