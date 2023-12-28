babf3c880cfac7371355484373f7eff9d8fe3af0
Klaus Ferreira <klauseverwalkingdev@yandex.com>
2023-12-28 10:40:29 -0300

    feature: create blog feature

---
+++ src/app/features/blog/blog.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterModule } from '@angular/router'
+
+@Component({
+	selector: 'dcorg-blog',
+	standalone: true,
+	imports: [RouterModule],
+	template: `<router-outlet></router-outlet>`,
+	styles: ``,
+	changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class BlogComponent {}
