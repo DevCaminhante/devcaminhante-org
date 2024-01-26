06989e40c8fa585490a767523e4bf52b08ba2b1b
Klaus Ferreira <klauseverwalkingdev@yandex.com>
2024-01-25 23:06:00 -0300

    feature: add first article and update blog routes

--- src/app/features/blog/blog.routes.ts
+++ src/app/features/blog/blog.routes.ts
@@ -1,6 +1,17 @@
 /* eslint-disable sort-keys-fix/sort-keys-fix */
 import {Route} from '@angular/router'
 
+const blogArticleData = {
+	articles: [
+		{
+			author: 'Klaus Ferreira',
+			date: '2021-07-07',
+			slug: '2021-07-07_e-nos-tornamos-devcaminhantes',
+			title: '...E nos tornamos DevCaminhantes'
+		}
+	]
+}
+
 export const blogRoutes: Route[] = [
 	{
 		path: '',
@@ -9,9 +20,17 @@ export const blogRoutes: Route[] = [
 	},
 	{
 		path: 'articles',
+		data: blogArticleData,
 		loadComponent: () =>
 			import('./article-list/article-list.component').then(
 				(m) => m.ArticleListComponent
+			)
+	},
+	{
+		path: 'articles/:slug',
+		loadComponent: () =>
+			import('./article-details/article-details.component').then(
+				(m) => m.ArticleDetailsComponent
 			)
 	}
 ]
